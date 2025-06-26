aa348d8a2eab5235e88eca25f8b60528e23728e1
mkit359 <meganlynn3493@aol.com>
2025-06-25 21:41:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ _steps = [
 
 
 # This automatically reads in the configuration
-@hydra.main(config_path="config", config_name="config", version_base=None)
+@hydra.main(config_name='config')
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
