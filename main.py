fc602f1e97d2de190443f0c0649a435498a48e5c
mkit359 <meganlynn3493@aol.com>
2025-06-25 21:44:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
                 "main",
+                env_manager="conda",
                 parameters={
                     "input_artifact": "sample.csv:latest",  
                     "output_artifact": "cleaned_sample.csv",
