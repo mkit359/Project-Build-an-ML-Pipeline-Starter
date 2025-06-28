012edfe474e97c314ad59d7a402f664e9237fab7
mkit359 <meganlynn3493@aol.com>
2025-06-27 22:29:04 -0400

    Update test_data.py

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,8 +60,8 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-def test_row_count(data):
+def test_row_count(data: pd.DataFrame):
     assert 15000 < data.shape[0] < 1000000
 
-def test_price_range(data, min_price, max_price):
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
     assert data['price'].between(min_price,max_price)
